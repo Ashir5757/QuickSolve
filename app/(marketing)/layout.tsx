e2eb2f05f01912c00c53fd5ebaf4490a00dc2934
Ashir5757 <ashirabbasi5757@gmail.com>
2026-03-17 23:30:03 +0500

    feat: initial commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
+export default function MarketingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className=''>
+
+    </div> 
+  )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Marketing main page  </div>
+  )
+}
+
+export default page
